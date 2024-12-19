3479ea36267c0beb6021f88a14b5dcc8b1166ac7
Maxsilver <142869326+Maxsilver46@users.noreply.github.com>
2024-12-19 22:09:12 +0530

    added sort function

--- evenOdd.js
+++ evenOdd.js
@@ -1,2 +1,19 @@
 const numbers = [3, 2, 10, 1, 9, 30, 41, 7, 12, 6];
 
+const numbersSort = numbers.toSorted((a, b) => {
+  return a - b;
+});
+
+const isEven = function (number) {
+  return number % 2 === 0;
+};
+
+const sortedNumbers = numbersSort.toSorted((a, b) => {
+  if (isEven(a) === isEven(b)) {
+    return 0;
+  }
+
+  return isEven(a) ? -1 : 1;
+});
+
+console.log(sortedNumbers);
